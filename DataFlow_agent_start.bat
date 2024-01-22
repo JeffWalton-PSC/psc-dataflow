5c58cbaf9e80a2900574331d8d52dc18682c8bae
Jeff Walton <jwalton@paulsmiths.edu>
2024-01-22 12:09:36 -0500

    update Prefect

--- DataFlow_agent_start.bat
+++ DataFlow_agent_start.bat
@@ -12,14 +12,9 @@ cd F:\Applications\psc-dataflow
 echo "cd F:\Applications\psc-dataflow" 1>> F:\Applications\psc-dataflow\logs\agent_out.log 2>&1
 
 echo "Set environment variables" 1>> F:\Applications\psc-dataflow\logs\agent_out.log 2>&1
-set "PREFECT_API_URL=http://psc-data:4200/api"
 set "PREFECT_HOME=F:\Applications\psc-dataflow\data\.prefect"
-set "PREFECT_LOCAL_STORAGE_PATH=F:\Applications\psc-dataflow\data\storage"
-set "PREFECT_LOGGING_LEVEL=DEBUG"
-set "PREFECT_ORION_API_HOST=psc-data"
-set "PREFECT_ORION_DATABASE_CONNECTION_URL=sqlite+aiosqlite:////Applications/psc-dataflow/data/orion.db"
 
-call C:\ProgramData\Anaconda3\condabin\activate.bat py310prefect 1>> F:\Applications\psc-dataflow\logs\agent_out.log 2>&1
+call C:\ProgramData\Anaconda3\condabin\activate.bat py311prefect 1>> F:\Applications\psc-dataflow\logs\agent_out.log 2>&1
 echo "call activate" 1>> F:\Applications\psc-dataflow\logs\agent_out.log 2>&1
 rem call conda list 1>> F:\Applications\psc-dataflow\logs\agent_out.log 2>&1
 
---
+++ DataFlow_server_start.bat
@@ -0,0 +1,27 @@
+@echo on
+echo "***** batch file running" >> F:\Applications\psc-dataflow\logs\server_out.log
+date /T >> F:\Applications\psc-dataflow\logs\server_out.log
+time /T >> F:\Applications\psc-dataflow\logs\server_out.log
+
+whoami 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+echo "whoami" 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+
+F:
+echo "F:" 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+cd F:\Applications\psc-dataflow
+echo "cd F:\Applications\psc-dataflow" 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+
+echo "Set environment variables" 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+set "PREFECT_HOME=F:\Applications\psc-dataflow\data\.prefect"
+
+call C:\ProgramData\Anaconda3\condabin\activate.bat py311prefect 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+echo "call activate" 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+call conda list 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+
+echo "prefect server start " >> F:\Applications\psc-dataflow\logs\server_out.log
+prefect server start 1>> F:\Applications\psc-dataflow\logs\server_out.log 2>&1
+
+rem pause
+time /T >> F:\Applications\psc-dataflow\logs\server_out.log
+echo "***** batch file exiting" >> F:\Applications\psc-dataflow\logs\server_out.log
+echo "****************************************************************" >> F:\Applications\psc-dataflow\logs\server_out.log
